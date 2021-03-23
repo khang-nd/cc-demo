017331116e29c6a28319b9492dc9916d4a81ff81
khang-nd <khang.nguyenduyvn@gmail.com>
2021-03-23 22:39:58 +0700

    .

--- src/settings.01871.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:false,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/GamePlay.fire",orientation:"landscape",jsList:[],bundleVers:{internal:"45e63",main:"75884"}};
